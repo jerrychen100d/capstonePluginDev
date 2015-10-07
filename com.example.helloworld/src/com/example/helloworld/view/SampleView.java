a138d7dd6a7c471ab654c456c1d932c31c5650aa
Jerry Chen <jerrychen100d@gmail.com>
2015-10-06 17:20:11 -0700

    moved files aroundin view folder

---
+++ com.example.helloworld/src/com/example/helloworld/view/SampleView.java
@@ -0,0 +1,24 @@
+package com.example.helloworld.view;
+
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.ui.part.ViewPart;
+
+public class SampleView extends ViewPart {
+
+	public SampleView() {
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void createPartControl(Composite parent) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void setFocus() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
